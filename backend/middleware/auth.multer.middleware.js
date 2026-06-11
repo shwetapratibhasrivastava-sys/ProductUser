3b71c64c0edcf6c4fae8f94bf7af9c92fc2a6300
Shweta Srivastava <shwetapratibhasrivastava@gmail.com>
2026-06-11 11:35:30 +0530

    updated

--- backend/middleware/auth.multer.middleware.js
+++ backend/middleware/auth.multer.middleware.js
@@ -1,14 +1,6 @@
 import multer from "multer";
-import { CloudinaryStorage } from "multer-storage-cloudinary";
-import cloudinary from "../config/cloudinary.js";
 
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "contactform",
-    allowed_formats: ["jpg", "jpeg", "png"],
-  },
-});
+const storage = multer.diskStorage({});
 
 const upload = multer({
   storage,
